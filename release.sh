c419c4f538f3cff9cc7ffd7e874aa84433f41bbd
Jørgen Teunis <jorgen@solvebright.com>
2022-05-16 11:32:03 +0200

    1.0.1

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 rm KnowledgeBaseApiModule-*.zip
-zip -r KnowledgeBaseApiModule-1.0.0.zip KnowledgeBaseApiModule -x "*.DS_Store" -x ".git*" -x ".idea*" -x "*.gitkeep"
+zip -r KnowledgeBaseApiModule-1.0.1.zip KnowledgeBaseApiModule -x "*.DS_Store" -x ".git*" -x ".idea*" -x "*.gitkeep"
